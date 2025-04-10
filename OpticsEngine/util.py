e3c90dcc716c22eae0f484aa5931250c11eda13b
Robert Kubasiak <robert2006goetel@gmail.com>
2025-04-10 22:13:59 +0200

    Add Winding Number algorithm to determine point-in-polygon

---
+++ OpticsEngine/util.py
@@ -0,0 +1,28 @@
+"""
+The Winding Number algorithm determines whether a point lies inside a polygon by calculating how many times the polygon winds around the point.
+
+Algorithm Description
+    - For each edge of the polygon, compute the angle subtended at P(x,y).
+    - Sum these angles; if the total is 2π, P lies inside; if 0, P lies outside.
+"""
+def is_point_inside_polygon(point, polygon):
+    x, y = point
+    wn = 0  # Winding number
+
+    for i in range(len(polygon)):
+        x1, y1 = polygon[i]
+        x2, y2 = polygon[(i + 1) % len(polygon)]
+        if y1 <= y:
+            if y2 > y and (x2 - x1) * (y - y1) - (x - x1) * (y2 - y1) > 0:
+                wn += 1
+        else:
+            if y2 <= y and (x2 - x1) * (y - y1) - (x - x1) * (y2 - y1) < 0:
+                wn -= 1
+    return wn != 0
+
+
+print(
+    is_point_inside_polygon([-5, 2.1], [
+        [-5, 5], [-5, -5], [5, -5], [5, 5], [-7, 7], [-7, 3], [-5, 3], [-5, 7], [-7, 5]
+    ])
+)
