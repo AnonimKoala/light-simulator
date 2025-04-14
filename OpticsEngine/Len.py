7b94591f0be1797afe9d894d82ace219b36cfd4b
Robert Kubasiak <robert2006goetel@gmail.com>
2025-04-14 18:46:41 +0200

    Len's property, calc logic init

--- OpticsEngine/Len.py
+++ OpticsEngine/Len.py
@@ -1,21 +1,15 @@
-from Geometry import Circle, Point
-from Material import Material
-from OpticsSolver import OpticsSolver
+from sympy import Point2D, cos, sin, pi
+from OpticsEngine.BasicObject import BasicObject
+from OpticsEngine.LightRay import LightRay
 
 
-class Len:
+class Len(BasicObject):
     """
     The class allows creating lenses.
     """
 
-    # Domyślna grubość
-    INIT_DIAMETER = 10
-
-    # Domyślny promień wewnętrzny soczewki
-    INIT_INNER_RADIUS = INIT_DIAMETER / 2
-
-    # Domyślna krzywizna
-    INIT_CURVATURE = 5
+    DEFAULT_RADIUS = 5
+    DEFAULT_HEIGHT = 20
 
     def __init__(self, x, y):
         """
@@ -28,120 +22,123 @@ class Len:
         :type y: float
         """
 
-        # Set the position of the lens
-        self.pos = Point(x, y)
+        self._pos = Point2D(x, y)
+        self._height = Len.DEFAULT_HEIGHT
+        self._rotation = pi / 2  # Rotation in radians about the OX axis
+        self._d = Len.DEFAULT_RADIUS * 2  # The thickness of the len
+        self._left_radius = Len.DEFAULT_RADIUS
+        self._right_radius = Len.DEFAULT_RADIUS
 
-        # Set the default diameter of the lens
-        self.d = Len.INIT_DIAMETER
+        self._left_curve = None
+        self._right_curve = None
+        self._vertices = {}  # Stores boundary points of the lens
+        self._curve_vertices = {}  # Stores middle top/bottom points of curves
 
-        # Set the left and right curvature radii of the lens
-        self.lRadius = Len.INIT_CURVATURE
-        self.rRadius = Len.INIT_CURVATURE
+        self.calc()
 
-        # Set the left and right inner radii of the lens
-        self.lInner = Len.INIT_INNER_RADIUS
-        self.rInner = Len.INIT_INNER_RADIUS
+    def get_collision_point(self, ray: LightRay) -> Point2D:
+        pass
 
-        # Set the material of the lens to glass
-        self.material = Material.glass()
-
-        # Initialize the equations for the left and right circles of the lens
-        self.lEq = None
-        self.rEq = None
-        self.calcEqs()
-
-        # Get a identifier for the lens and add it to the OpticsSolver's lens list
-        self.uid = OpticsSolver.getNextID()
-        OpticsSolver.LENS.append(self)
-
-    def calcEqs(self):
+    def calc(self):
         """
-        Calculates and updates the equations of the circles
-        that form the lens.
+        Calculates and updates the len.
         """
-        self.lEq = Circle(
-            self.pos.x - self.lRadius + self.lInner,
-            self.pos.y,
-            self.lRadius
-        )
 
-        self.rEq = Circle(
-            self.pos.x + self.rRadius - self.rInner,
-            self.pos.y,
-            self.rRadius
-        )
+        d2sin = (self.d / 2) * sin(self.rotation)
+        d2cos = (self.d / 2) * cos(self.rotation)
+        h2sin = (self.height / 2) * sin(self.rotation)
+        h2cos = (self.height / 2) * cos(self.rotation)
 
-    def scale(self, scale):
+        self._vertices = {  # Stores the vertices of the len
+            "top-left": Point2D(self.pos.x - d2cos - h2sin, self.pos.y - d2sin + h2cos),
+            "top-right": Point2D(self.pos.x + d2cos - h2sin, self.pos.y + d2sin + h2cos),
+            "bottom-left": Point2D(self.pos.x - d2cos + h2sin, self.pos.y - d2sin - h2cos),
+            "bottom-right": Point2D(self.pos.x + d2cos + h2sin, self.pos.y + d2sin - h2cos),
+        }
+
+        rest2d = (self.d - self.left_radius - self.right_radius) / 2
+        d2rest_cos = (self.d / 2 + rest2d) * cos(self.rotation)
+        d2rest_sin = (self.d / 2 + rest2d) * sin(self.rotation)
+
+        self._curve_vertices = {  # Stores middle top/bottom points of curves
+            "left-top": Point2D(self.pos.x - d2rest_cos - h2sin, self.pos.y - d2rest_sin + h2cos),
+            "left-bottom": Point2D(self.pos.x - d2rest_cos + h2sin, self.pos.y - d2rest_sin - h2cos),
+            "right-top": Point2D(self.pos.x + d2rest_cos - h2sin, self.pos.y + d2rest_sin + h2cos),
+            "right-bottom": Point2D(self.pos.x + d2rest_cos + h2sin, self.pos.y + d2rest_sin - h2cos),
+        }
+
+    def scale(self, scale_factor: float):
         """
-        Scales the dimensions of the lens.
-
-        :param scale: Target scale
-        :type scale: float
+        Scales the dimensions of the len.
         """
-        self.d *= scale
-        self.lInner = self.d / 2
-        self.rInner = self.d / 2
+        self.d *= scale_factor
+        self.height *= scale_factor
+        self.left_radius *= scale_factor
+        self.right_radius *= scale_factor
+        self.calc()
 
-        self.calcEqs()
+    @property
+    def pos(self) -> Point2D:
+        return self._pos
 
-    def setPos(self, x, y):
-        """
-        Moves the lens and updates its equations.
+    @pos.setter
+    def pos(self, point: Point2D):
+        self._pos = point
+        self.calc()
 
-        :param x: X-coordinate
-        :type x: float
+    @property
+    def height(self) -> float:
+        return self._height
 
-        :param y: Y-coordinate
-        :type y: float
-        """
-        self.pos = Point(x, y)
-        self.calcEqs()
+    @height.setter
+    def height(self, height: float):
+        self._height = height
+        self.calc()
 
-    def setD(self, d):
-        """
-        Sets the thickness of the lens and scales its dimensions.
+    @property
+    def rotation(self) -> float:
+        return self._rotation
 
-        :param d: New thickness
-        :type d: float
-        """
-        self.scale(d / self.d)
+    @rotation.setter
+    def rotation(self, rotation: float):
+        self._rotation = rotation
+        self.calc()
 
-    def setLRadius(self, r):
-        """
-        Sets the left curvature radius and updates the lens equations.
+    @property
+    def d(self) -> float:
+        return self._d
 
-        :param r: Left curvature radius
-        :type r: float
-        """
-        self.lRadius = r
-        self.calcEqs()
+    @d.setter
+    def d(self, d: float):
+        if d < self.left_radius + self.right_radius:
+            raise ValueError("The thickness of the lens must be greater than the sum of the radii.")
+        if d < 0:
+            raise ValueError("The thickness of the lens must be a positive value.")
+        self._d = d
+        self.calc()
 
-    def setRRadius(self, r):
-        """
-        Sets the right curvature radius and updates the lens equations.
+    @property
+    def left_radius(self) -> float:
+        return self._left_radius
 
-        :param r: Right curvature radius
-        :type r: float
-        """
-        self.rRadius = r
-        self.calcEqs()
+    @left_radius.setter
+    def left_radius(self, radius: float):
+        self._left_radius = radius
+        self.calc()
 
-    def setLInner(self, inner):
-        """
-        Sets the distance from the center to the left edge of the lens,
-        scaling its dimensions.
+    @property
+    def right_radius(self) -> float:
+        return self._right_radius
 
-        :param inner: Distance
-        :type inner: float
-        """
-        self.scale(inner / self.lInner)
+    @right_radius.setter
+    def right_radius(self, radius: float):
+        self._right_radius = radius
+        self.calc()
 
-    def setRInner(self, inner):
-        """
-        Sets the distance from the center to the right edge of the lens,
-        scaling its dimensions.
+    @property
+    def vertices(self) -> dict:
+        return self._vertices
 
-        :param inner: Distance
-        :type inner: float
-        """
-        self.scale(inner / self.rInner)
+    @property
+    def curve_vertices(self) -> dict:
+        return self._curve_vertices
