52981a759095206533952612bb15adc5239edca2
Robert Kubasiak <robert2006goetel@gmail.com>
2025-05-08 16:55:49 +0200

    rename collision_detector() + logic, new nearest_to_origin()

--- optics/RayController.py
+++ optics/RayController.py
@@ -11,18 +11,31 @@ class RayController:
         self._start_point = Point2D(start_point.x(), start_point.y())
         self._end_point = None
         self._angle_deg = 0.0
-        self.ray = None
+        self.ray = SympyRay(self.start_point, angle=self.angle_rad)
 
     def update_props(self, start_point: QPointF, angle_deg: float):
         self.start_point = Point2D(start_point.x(), start_point.y())
         self.angle_deg = angle_deg
         self.ray = SympyRay(self.start_point, angle=self.angle_rad)
 
-    def get_refractions(self) -> list[QPointF] | None:
-        Solver.collision_detector(self)
+    def get_refractions(self) -> QPointF | None:
+        return Solver.find_first_collision(self)
+
+    def first_intersection(self, obj):
+        if intersections := self.ray.intersection(obj):
+            return Solver.nearest_to_origin(self.start_point, intersections)
+        return None
+
+    def intersections(self, obj):
+        if intersections := self.ray.intersection(obj):
+            return intersections
+        return None
+
+    def distance(self, obj):
+        return self.ray.distance(obj)
 
     @property
-    def start_point(self):
+    def start_point(self) -> Point2D:
         return self._start_point
 
     @start_point.setter
--- optics/Solver.py
+++ optics/Solver.py
@@ -1,17 +1,37 @@
+from sympy import Point2D
 from optics.BasicObject import BasicObject
 
 
 class Solver:
     optical_objects: list[BasicObject] = []
-
+    lasers = []
     @staticmethod
-    def collision_detector(ray):
+    def find_first_collision(ray) -> Point2D|None:
         """
         Detects the collision of a ray with optical objects.
         :param ray: The ray to check for collisions
         :type ray: RayController
         """
-
+        collision_points = []
         for obj in Solver.optical_objects:
-            if obj.get_collisions(ray):
-                pass
+            if collision_point := obj.get_collision(ray):
+                collision_points.append(collision_point)
+
+        if collision_points:
+            return Solver.nearest_to_origin(ray.start_point, collision_points)
+        return None
+
+    @staticmethod
+    def nearest_to_origin(origin, objs):
+        """
+        Finds the nearest object to the origin.
+        :param origin: The origin point
+        :type origin: Point2D
+        :param objs: List of objects to check
+        :type objs: list[Point2D]
+        :return: The nearest object to the origin
+        """
+        return min(objs, key=lambda obj: obj.distance(origin))
+
+
+
