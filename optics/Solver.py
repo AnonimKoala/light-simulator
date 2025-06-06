bb894e8a173b2f597e93f11cda47b0a417b2fe5b
Robert Kubasiak <robert2006goetel@gmail.com>
2025-06-06 09:44:39 +0200

    Len: updating after timeout, using formula for Ellipse eq and intersections

--- optics/Solver.py
+++ optics/Solver.py
@@ -1,4 +1,5 @@
-from sympy import Point2D, Segment2D, Line2D, Ray, Ray2D, pi, cos, sin, solve, Ellipse, symbols
+from sympy import Point2D, Segment2D, Line2D, Ray, Ray2D, pi, cos, sin, solve, Eq, tan
+from sympy.abc import x, y
 from sympy.geometry.entity import GeometrySet
 
 from conf import RAY_MAX_LENGTH, MAX_REFRACTIONS
@@ -101,6 +102,13 @@ class Solver:
             x, y = symbols('x y')
             print([Point2D(sol[x], sol[y]) for sol in solutions]) # fixme
             return [Point2D(sol[x], sol[y]) for sol in solutions]
+        print("obj: ", obj)
+        if not isinstance(obj, GeometrySet):  # For Eq objects like Ellipse.equation()
+            A, B, C = Line2D(*ray.points).coefficients
+            line_eq = Eq(A * x + B * y + C, 0)
+            points = Solver.solve_safe(line_eq, obj)
+            return points
+
         if intersections := ray.intersection(obj):
             if any(not isinstance(i, Point2D) for i in intersections):
                 raise NotImplementedError("all_intersections process only Point2D")
@@ -113,3 +121,38 @@ class Solver:
         end_x = ray.source.x + RAY_MAX_LENGTH * cos(ray_angle)
         end_y = ray.source.y + RAY_MAX_LENGTH * sin(ray_angle)
         return round_point(Point2D(end_x, end_y))
+
+    @staticmethod
+    def solve_safe(obj1, obj2):
+        """
+        Solves the intersection of two objects, type similar, safely.
+        :param obj1: The first object
+        :param obj2: The second object
+        :return: The intersection points or None if no intersection
+        """
+        solutions = solve([obj1, obj2], (x, y), dict=True)
+        real_points = []
+        for sol in solutions:
+            x_val = list(sol.values())[0].evalf()
+            y_val = list(sol.values())[1].evalf()
+            if x_val.is_real and y_val.is_real:
+                real_points.append(Point2D(x_val, y_val))
+        return real_points
+
+    @staticmethod
+    def calc_ellipse_eq(pos_x: float, pos_y: float, h_radius: float, v_radius: float, theta: tan) -> Eq:
+        """
+        Calculates the equation of an ellipse.
+        :param pos_x: X-coordinate of the ellipse center
+        :param pos_y: Y-coordinate of the ellipse center
+        :param h_radius: Horizontal radius of the ellipse
+        :param v_radius: Vertical radius of the ellipse
+        :param theta: Tangens of rotation angle in radians
+        :return: The equation of the ellipse
+        """
+        if h_radius <= 0 or v_radius <= 0:
+            raise ValueError("The radii of the lens must be positive values.")
+        if theta == 0:
+            return Eq((x - pos_x) ** 2 / (h_radius ** 2) + (y - pos_y) ** 2 / (v_radius ** 2), 1)
+        return Eq(-1 + (-pos_y - theta * (-pos_x + x) + y) ** 2 / (v_radius ** 2 * (theta ** 2 + 1)) + (
+                -pos_x + theta * (-pos_y + y) + x) ** 2 / (h_radius ** 2 * (theta ** 2 + 1)), 0)
